1bfc714260858f420c3e8a554abd669e775a68bc
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-06-30 17:55:37 -0300

    Emoji font

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "UbuntuMono Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font2[] = { "Symbola:pixelsize=10" };
 static int borderpx = 2;
 
 /*
